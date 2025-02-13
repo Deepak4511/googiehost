916147bb01444f582ff857cdd44d2fc562249021
deepak4511 <deepaksingh451181@gmail.com>
2025-02-13 09:17:58 +0530

    fix responsive

--- inc/hosting-plan.php
+++ inc/hosting-plan.php
@@ -1,29 +1,29 @@
 <div class="row justify-content-center">
     <!-- Free Hosting Plan -->
-    <div class="col-md-4 mb-4">
+    <div class="col-lg-4 col-md-6 mb-4">
         <div class="pricing-card h-100">
             <div class="plan-image">
                 <img src="images/icons/FreeIcon.png" alt="free web hosting" class="img-fluid">
             </div>
             <div class="pricing-header">Free Web Hosting</div>
             <div class="pricing-subheader">100% Free No Card, No Ads & No Hassle</div>
-            <a href="checkout" class="btn btn-outline-dark pricing-button mb-4 w-100 rounded-3 p-3 fw-bold">Proceed with Free</a>
+            <a href="checkout" class="btn btn-outline-dark free-button mb-4 w-100 rounded-3 p-3 fw-bold">Proceed with Free</a>
             <ul class="feature-list text-start">
-                <li>Host 1 Website</li>
-                <li>MB SSD Storage</li>
-                <li>Free SSL Certificate</li>
-                <li>100 GB Bandwidth</li>
-                <li>WordPress Manager</li>
-                <li>Business Email</li>
-                <li>MySQL, FTP Account*</li>
-                <li>Cloudflare Protection</li>
-                <li>99% Uptime</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Host 1 Website</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">MB SSD Storage</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Free SSL Certificate</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">100 GB Bandwidth</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">WordPress Manager</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Business Email</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">MySQL, FTP Account*</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Cloudflare Protection</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">99% Uptime</li>
             </ul>
         </div>
     </div>
     <!-- Paid Hosting Plan -->
-    <div class="col-md-4 mb-4">
-        <div class="pricing-card h-100">
+    <div class="col-lg-4 col-md-6 mb-4">
+        <div class="pricing-card pro-pricing-card h-100">
             <div class="plan-image">
                 <img src="images/icons/BusinessIcon.png" alt="lifetime web hosting" class="img-fluid">
             </div>
@@ -31,16 +31,16 @@
             <div class="pricing-subheader">Pro (Lifetime Deal) Starting at $25</div>
             <a href="checkout" class="btn pricing-button mb-4 w-100 rounded-3 p-3 fw-bold">Proceed with Paid</a>
             <ul class="feature-list text-start">
-                <li>Number of Websites 3</li>
-                <li>10 GB NVMe SSD</li>
-                <li>Unmetered Bandwidth</li>
-                <li>3 Email Accounts</li>
-                <li>3 MySQL Database</li>
-                <li>1 FTP Account</li>
-                <li>SSL Certificate</li>
-                <li>Softaculous Auto Installer</li>
-                <li>LS Cache Included</li>
-                <li>99.95% Uptime</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Number of Websites 3</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">10 GB NVMe SSD</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Unmetered Bandwidth</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">3 Email Accounts</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">3 MySQL Database</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">1 FTP Account</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">SSL Certificate</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">Softaculous Auto Installer</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">LS Cache Included</li>
+                <li><img src="https://googiehost.com/images/icons/Check.png" alt="" class="me-3">99.95% Uptime</li>
             </ul>
         </div>
     </div>
