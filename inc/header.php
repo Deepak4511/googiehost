703cbeede60cc83bc69e528316d97e2a85ad8bdb
BanshwarTech <banshwaralekh6569@gmail.com>
2025-02-13 14:11:28 +0530

    row margin-right

--- inc/header.php
+++ inc/header.php
@@ -23,7 +23,6 @@ if (!isset($page)) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <title>Header</title>
     <link href="/googiehost/assets/css/stylesheet.css" rel="stylesheet" />
 </head>
